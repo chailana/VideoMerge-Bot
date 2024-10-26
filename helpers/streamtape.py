cc73df092db488fa88fef96fd274d8e063042b2f
chailana <chailana35217793@gmail.com>
2024-10-26 21:16:10 +0530

    Update streamtape.py

--- helpers/streamtape.py
+++ helpers/streamtape.py
@@ -1,5 +1,5 @@
 # (c) @AbirHasan2005
-
+from pyrogram.enums import ParseMode
 import aiohttp
 from configs import Config
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
@@ -19,7 +19,7 @@ async def UploadToStreamtape(file: str, editable: Message, file_size: int):
             download_link = data_f["result"]["url"]
             filename = file.split("/")[-1].replace("_", " ")
             text_edit = f"File Uploaded to Streamtape!\n\n**File Name:** `{filename}`\n**Size:** `{humanbytes(file_size)}`\n**Link:** `{download_link}`"
-            await editable.edit(text_edit, parse_mode="Markdown", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=download_link)]]))
+            await editable.edit(text_edit, parse_mode=ParseMode.HTML, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=download_link)]]))
     except Exception as e:
         print(f"Error: {e}")
         await editable.edit("Sorry, Something went wrong!\n\nCan't Upload to Streamtape. You can report at [Support Group](https://t.me/linux_repo)")
